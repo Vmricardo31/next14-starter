2a984d34969af39dc5913bc2f0bab1102a324ca9
VMR <vmricardo31@outlookcom>
2025-03-16 22:37:54 +0000

    avances

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
   const not_found = () => {
     return(
-      <div>not-found</div>
+      <div>not-found
+      <p>Sorry, page not found</p>
+      <Link href="/">Go back to the homepage</Link>
+      </div>
   )
    }
     
